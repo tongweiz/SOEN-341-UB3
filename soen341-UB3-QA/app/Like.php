c03e2a06bf56fa74ab2818b846fdaabde7692f07
Tripe2000 <tripe2000@gmail.com>
2018-02-08 01:40:37 -0500

    Created a new Like model, as well as a likeable table. Also installed composer

---
+++ soen341-UB3-QA/app/Like.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Like extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'likeables';
+
+    protected $fillable = [
+        'user_id',
+        'likeable_id',
+        'likeable_type',
+    ];
+}
