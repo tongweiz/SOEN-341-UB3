a0a9808a8ee18d1c2c136e948941df34faa822f9
Alex <35375322+Alex44711@users.noreply.github.com>
2018-02-15 11:58:19 -0500

    Update ExampleTest.php

--- soen341-UB3-QA/tests/Feature/ExampleTest.php
+++ soen341-UB3-QA/tests/Feature/ExampleTest.php
@@ -3,8 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\RefreshDatabase
 
 class ExampleTest extends TestCase
 {
@@ -16,8 +15,6 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
-        $this->withoutMiddleware();
 
         $response->assertStatus(200);
     }
