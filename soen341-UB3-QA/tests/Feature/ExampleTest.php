9f7e92ca117584c30c284acbee16deae27d555a5
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-02-24 17:34:50 -0500

    fix sample dusk test

--- soen341-UB3-QA/tests/Feature/ExampleTest.php
+++ soen341-UB3-QA/tests/Feature/ExampleTest.php
@@ -10,13 +10,13 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic test example that checks that you get to home page and you dont see the default laravel page.
+     * We already modified the home page.
      *
      * @return void
      */
     public function testBasicTest()
     {
-        $this->visit('/')
-            ->see('Laravel');
+        $this->visit('/')->dontSee('Laravel');
     }
 }
