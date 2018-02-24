f7f5eeb47abb032c191c2bb774c72317863565b6
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-02-24 16:54:52 -0500

    testing first register test. issue #79

---
+++ soen341-UB3-QA/tests/Feature/authTests.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTests
+{
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+   /**
+    * Test the case where a user registers successfully
+    * Confirms that the user is redirected to /home.
+    * Confirms the user exists in the database.
+    * Confirms the user is logged in.
+    */
+    public function testRegistrationSuccess(){
+        $this->visit('/register')
+            ->type('tester', 'name')
+            ->type('tester@gmail.com', 'email')
+            ->type('secret123', 'password')
+            ->type('secret123', 'password_confirmation')
+            ->press('Register')
+            ->seePageIs('http://localhost/home')
+            ->seeInDatabase('users', [
+                'email' => 'tester@gmail.com'
+            ]);
+    }
+}
