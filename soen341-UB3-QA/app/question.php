12cfb5bd40ee8ed631f00be96678fedef06b6eba
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-02-26 12:10:25 -0500

    added fillable attribute to question model

--- soen341-UB3-QA/app/question.php
+++ soen341-UB3-QA/app/question.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class question extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'user_id',
+    ];
+
     public function users()
     {
         return $this->belongsTo('app\Users');
