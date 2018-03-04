ce1f09ec19c99a0aae3cfce4d7ce095e6e273d4e
Alex <secondsmall86@gmail.com>
2018-03-04 15:16:36 -0500

    show labels in homepage

--- soen341-UB3-QA/resources/views/welcome.blade.php
+++ soen341-UB3-QA/resources/views/welcome.blade.php
@@ -50,8 +50,15 @@
                             <p class="card-text" style="margin-top: -20px; margin-bottom: 20px">
                                 <?php $parts = explode('.', $data->content); echo($parts[0] . '...')?></p>
 
-                            <!--Display labels of question (TO MODIFY WITH DB DATA)-->
-                            <span id="labels-styled">LABEL 1</span> <span id="labels-styled">LABEL 2</span>
+                               <!--Display labels of question-->
+                            @if((!$data->labels) == "")
+                                <?php $parts = explode(',', $data->labels)?>
+                                @foreach($parts as $label)
+                                <span id="labels-styled">
+                                    {{$label}}
+                                </span>
+                                @endforeach
+                            @endif
 
                             <!--Display read more button-->
                             <a href="/question/{{$data->id}}" class="btn btn-primary" name="Read" style="float: right;">Read
