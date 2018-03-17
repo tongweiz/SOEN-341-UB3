e0c8806bafc31e4e329b2efdf66d54d7bd8072d0
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-03-17 13:21:55 -0400

    checking if browsertestkit still runs on travisCI

--- soen341-UB3-QA/tests/Browser/ReplyTest.php
+++ soen341-UB3-QA/tests/Browser/ReplyTest.php
@@ -3,36 +3,33 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\BrowserKitTestCase;
 use App\Question;
 use App\User;
 
-class ReplyTest extends DuskTestCase
+//the reply tests do not use javascript so it will continue to use BrowserKitTestCase
+class ReplyTest extends BrowserKitTestCase
 {
-    //will rollback db after every test
-    //When run LOCALLY: have the db and tables setup but no data.
     use DatabaseMigrations;
+    use DatabaseTransactions;
 
     //set up environment for tests
     public function setUp()
     {
         parent::setUp();
-
         //create new users
         factory(User::class)->create([
             'name' => 'user1',
             'email' => 'user1@gmail.com',
             'password' => 'secret1234',
         ]);
-
         factory(Question::class)->create([
             'title' => 'first title test',
             'content' => 'first content',
             'user_id' => 1,
             'nb_replies' => 1,
         ]);
-
         factory(User::class)->create([
             'name' => 'user2',
             'email' => 'user2@gmail.com',
@@ -44,66 +41,61 @@ class ReplyTest extends DuskTestCase
      * Test the case where a user is not authenticated
      * Stays on page.
      * Reply not added.
-     * @throws
      */
-   /* public function testReplyFailureNotAuthenticated()
+    public function testReplyFailureNotAuthenticated()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/question/1')
-                ->dump()
-                ->type('body', 'this is a reply')
-                ->press('Submit')
-                ->assertPathIs('/question/1')
-                ->assertSee('No comments')
-                ->assertDatabaseMissing('replies', [
-                    'content' => 'this is a reply']);
-        });
-    }*/
+        $this->dontSeeIsAuthenticated();
+        $this->visit('/question/1')
+            ->type('this is a reply', 'body')
+            ->press('Submit')
+            ->seePageIs('http://localhost/question/1')
+            ->see('No comments')
+            ->dontSeeInDatabase('replies', [
+                'content' => 'this is a reply']);
+    }
 
     /**
      * Test the case where a user is authenticated but wants to send an empty reply.
      * Stays on page.
      * Reply not added.
-     * @throws
      */
-    /*public function testReplyFailureEmptyBody()
+    public function testReplyFailureEmptyBody()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->loginAs(\App\User::find(2))
-                ->visit('/question/1')
-                ->press('Submit')
-                ->assertPathIs('/question/1')
-                ->assertSee('No comments')
-                ->assertAuthenticated()
-                ->assertDatabaseMissing('replies', [
-                    'content' => '']);
-        });
-    }*/
+        //login as user id 2
+        $user = \App\User::find(2);
+        $this->actingAs($user)
+            ->visit('/question/1')
+            ->press('Submit')
+            ->seePageIs('http://localhost/question/1')
+            ->see('No comments')
+            ->dontSeeInDatabase('replies', [
+                'content' => ''])
+            ->isAuthenticated();
+    }
 
     /**
      * Test the case where a user is authenticated and creates a reply successfully.
      * Stays on page.
      * Reply is added to page and database.
-     * @throws
      */
-    /*public function testReplySuccessfullReply()
+    public function testReplySuccessfullReply()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->loginAs(\App\User::find(2))
-                ->visit('/question/1')
-                ->type('body', 'this reply will be saved')
-                ->press('Submit')
-                ->assertPathIs('/question/1')
-                ->assertSee('this reply will be saved')
-                ->assertAuthenticated()
-                ->assertDatabaseHas('replies', [
-                    'id' => 1,
-                    'content' => 'saved',
-                    'question_id' => 1,
-                    'user_id' => 2,
-                    'likectr' => 0,
-                    'dislikectr' => 0,
-                    'status' => 0]);
-        });
-    }*/
+        //login as user id 2
+        $user = \App\User::find(2);
+        $this->actingAs($user)
+            ->visit('/question/1')
+            ->type('saved', 'body')
+            ->press('Submit')
+            ->seePageIs('http://localhost/question/1')
+            ->see('saved')
+            ->dontSee('No comments')
+            ->isAuthenticated();
+        $this->seeInDatabase('replies', ['id' => 1,
+            'content' => 'saved',
+            'question_id' => 1,
+            'user_id' => 2,
+            'likectr' => 0,
+            'dislikectr' => 0,
+            'status' => 0]);
+    }
 }
--- soen341-UB3-QA/tests/Browser/SearchTest.php
+++ soen341-UB3-QA/tests/Browser/SearchTest.php
@@ -3,36 +3,33 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\BrowserKitTestCase;
 use App\Question;
 use App\User;
 
-class SearchTest extends DuskTestCase
+//the search tests do not use javascript so it will continue to use BrowserKitTestCase
+class SearchTest extends BrowserKitTestCase
 {
-    //will rollback db after every test
-    //When run LOCALLY: have the db and tables setup but no data.
     use DatabaseMigrations;
+    use DatabaseTransactions;
 
     //set up environment for tests
     public function setUp()
     {
         parent::setUp();
-
-        //create new use
+        //create new user
         factory(User::class)->create([
             'name' => 'user1',
             'email' => 'user1@gmail.com',
-            'password' => 'secret123'
+            'password' => 'secret1234',
         ]);
-
         factory(Question::class)->create([
             'title' => 'first title test',
             'content' => 'first content',
             'user_id' => 1,
             'nb_replies' => 0,
         ]);
-
         factory(Question::class)->create([
             'title' => 'second title test',
             'content' => 'second content',
@@ -45,180 +42,149 @@ class SearchTest extends DuskTestCase
      * Test the case where a user's searches empty string in search bar
      * User stays on home page.
      * User should see both questions in database.
-     * @throws
      */
-  /*  public function testSearchEmpty()
+    public function testSearchEmpty()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', '')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->see('second title test');
+    }
 
     /**
      * Test the case where a user's searches a word that matches no title.
      * User stays on home page.
      * User should see no questions.
-     * @throws
      */
-    /*public function testSearchMatchWordZero()
+    public function testSearchMatchWordZero()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'elephant')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertDontSee('first title test')
-                ->assertDontSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('elephant', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->dontSee('first title test')
+            ->dontSee('second title test');
+    }
 
     /**
      * Test the case where a user's searches a word that matches only one question.
      * User stays on home page.
      * User should see first question.
-     * @throws
      */
     public function testSearchMatchWordOne()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->assertSee("Questions!")
-                ->type('search', 'first')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertDontSee('second title test');
-        });
+        $this->visit('/home')
+            ->type('first', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->dontSee('second title test');
     }
 
     /**
      * Test the case where a user's searches a word that matches all questions.
      * User stays on home page.
      * User should see both questions.
-     * @throws
      */
-   /* public function testSearchMatchWordAll()
+    public function testSearchMatchWordAll()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'test')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('test', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->see('second title test');
+    }
 
     /**
      * Test the case where a user's searches a part of a word that matches no question.
      * User stays on home page.
      * User should see no questions.
-     * @throws
      */
-   /* public function testSearchMatchPartWordZero()
+    public function testSearchMatchPartWordZero()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'el')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertDontSee('first title test')
-                ->assertDontSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('el', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->dontSee('first title test')
+            ->dontSee('second title test');
+    }
 
     /**
      * Test the case where a user's searches a part of a word that matches one question.
      * User stays on home page.
      * User should see only first question.
-     * @throws
      */
-    /*public function testSearchMatchPartWordOne()
+    public function testSearchMatchPartWordOne()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'ir')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertDontSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('ir', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->dontSee('second title test');
+    }
 
     /**
      * Test the case where a user's searches a part of a word that matches all questions.
      * User stays on home page.
      * User should sees all questions.
-     * @throws
      */
-   /* public function testSearchMatchPartWordAll()
+    public function testSearchMatchPartWordAll()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'es')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('es', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->see('second title test');
+    }
 
     /**
      * Test the case where a user's searches multiple words that matches no questions.
      * User stays on home page.
      * User should see no questions.
-     * @throws
      */
-    /*public function testSearchMatchMultipleWordsZero()
+    public function testSearchMatchMultipleWordsZero()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'test failure')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertDontSee('first title test')
-                ->assertDontSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('test failure', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->dontSee('first title test')
+            ->dontSee('second title test');
+    }
 
     /**
      * Test the case where a user's searches multiple words that matches one question only.
      * User stays on home page.
      * User should see first question.
-     * @throws
      */
-    /*public function testSearchMatchMultipleWordsOne()
+    public function testSearchMatchMultipleWordsOne()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'first title')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertDontSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('first title', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->dontSee('second title test');
+    }
 
     /**
      * Test the case where a user's searches multiple words that matches all questions.
      * User stays on home page.
      * User should see all questions.
-     * @throws
      */
-   /* public function testSearchMatchMultipleWordsAll()
+    public function testSearchMatchMultipleWordsAll()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/home')
-                ->type('search', 'title test')
-                ->press('Go!')
-                ->assertPathIs('/home')
-                ->assertSee('first title test')
-                ->assertSee('second title test');
-        });
-    }*/
+        $this->visit('/home')
+            ->type('title test', 'search')
+            ->press('Go!')
+            ->seePageIs('http://localhost/home')
+            ->see('first title test')
+            ->see('second title test');
+    }
 }
