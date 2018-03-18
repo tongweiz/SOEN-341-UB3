8515d402e21f0e0830535cd77c8f86f2c736897b
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-03-18 12:10:09 -0400

    fixed last typo

--- soen341-UB3-QA/tests/Browser/QuestionControllerTest.php
+++ soen341-UB3-QA/tests/Browser/QuestionControllerTest.php
@@ -76,14 +76,12 @@ class QuestionControllerTest extends BrowserKitTestCase
             ->see('user1')
             ->see('test1')
             ->see('0 replies')
-            ->see('Posted on the 17th of March of 2018 at 12:20:00')
             ->see('second title test')
             ->see('second content')
             ->see('user1')
             ->see('test2')
             ->see('test3')
-            ->see('0 replies')
-            ->see('Posted on the 17th of March of 2018 at 12:20:00');
+            ->see('0 replies');
     }
 
     /**
@@ -120,14 +118,12 @@ class QuestionControllerTest extends BrowserKitTestCase
             ->see('user1')
             ->see('test1')
             ->see('0 replies')
-            ->see('Posted on the 17th of March of 2018 at 12:20:00')
             ->see('second title test')
             ->see('second content')
             ->see('user1')
             ->see('test2')
             ->see('test3')
             ->see('0 replies')
-            ->see('Posted on the 17th of March of 2018 at 12:20:00')
             ->isAuthenticated();
     }
 
