99dfad9158147f285095fe547dc6af922ddf0d24
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-03-19 14:27:42 -0400

    trying to add a pause after page visit to let js load in dom.

--- soen341-UB3-QA/tests/Browser/AcceptRejectTest.php
+++ soen341-UB3-QA/tests/Browser/AcceptRejectTest.php
@@ -175,6 +175,7 @@ class AcceptRejectTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs(\App\User::find(1))
                 ->visit('/question/1')
+                ->pause(3000)
                 ->assertVisible('@accept-owner-2')
                 ->click('@accept-owner-2')
                 ->pause(3000);
