51d1061c4e0d2b6c5073de0870bc497025a710d6
Irina Patrocinio Frazao <irinafrazao@hotmail.com>
2018-03-19 15:26:16 -0400

    trying to dumo html of page

--- soen341-UB3-QA/tests/Browser/AcceptRejectTest.php
+++ soen341-UB3-QA/tests/Browser/AcceptRejectTest.php
@@ -179,11 +179,11 @@ class AcceptRejectTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->loginAs(\App\User::find(1))
-                ->visit('/question/1')
-                ->assertSee('Leave a Reply:')
-                ->assertVisible('@accept-owner-2')
-                ->click('@accept-owner-2')
-                ->pause(3000);
+                ->visit('/question/1')->dump();
+               //->assertSee('Leave a Reply:')
+               // ->assertVisible('@accept-owner-2')
+                //->click('@accept-owner-2')
+                //->pause(3000);
             //$this->assertDatabaseHas('replies', ['id' => 2, 'status' => 1]);
         });
     }
