eb8744a68ed7e36f8ac4a73c98d72f20cf73325d
Alireza <alirezak2009@hotmail.com>
2018-03-29 11:16:31 -0400

    User avatar created and selection in registration

--- soen341-UB3-QA/database/factories/UserFactory.php
+++ soen341-UB3-QA/database/factories/UserFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'avatar' => $faker->numberBetween(1,3),
         'remember_token' => str_random(10),
     ];
 });
